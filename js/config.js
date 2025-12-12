ca8272351d85ee916f8fc698c9eecf43ecb2d2e6
Süleyman Petek <suleymanpetek@yahoo.com>
2025-12-12 22:16:13 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_fgSchdRpr', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7fjv211mrglcf91h6b8kfo0tn9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_vqEOHeKjg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5vnb22v6afnpuljnsc8q694d35', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
